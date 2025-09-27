17ff2a0f663fe5c86205938cdaefd1b0791ead5f
Raymond <raymondhuang615@yahoo.com>
2025-09-27 00:35:38 -0700

    Adding live preview browser

---
+++ src/components/homepage.jsx
@@ -0,0 +1,16 @@
+// Open the Simple Browser in VS Code
+// Press Ctrl+Shift+P to open the Command Palette.
+// Type and select Simple Browser: Show.
+// Navigate to Your Localhost URL
+// In the Simple Browser panel that appears, type: http://localhost:5173/
+
+const Homepage = () => {
+    return (
+        <div>
+            <div>Hello World</div>
+        </div>
+
+    )
+}
+
+export default Homepage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Homepage from './components/homepage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
